abc226d46160978b17fd1913459942158c9a019b
DanielaAraujoZ <daraujoz@unal.edu.co>
2022-08-07 13:15:06 -0500

    Responsive design improvements

--- src/AdventuresApp.jsx
+++ src/AdventuresApp.jsx
@@ -30,8 +30,14 @@ function RegistrationApp() {
 
   return (
     <div className='w-full px-4 flex gap-16 container-all h-screen'>
+      <button type="button" class=' absolute top-0 mt-4 hover:text-gray-600 lg:hidden' data-hs-sidebar='#docs-sidebar' aria-controls='docs-sidebar' aria-label='Toggle navigation'>
+        <span class="sr-only">Toggle Navigation</span>
+        <svg class="w-7 h-7" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+        </svg>
+      </button>
       <SideMenu/>
-      <div className='container-content basis-4/5 my-16 mr-48 flex flex-col gap-5'>
+      <div className='container-content m-auto basis-4/5 my-16 lg:mr-48 flex flex-col gap-5'>
         <AdventuresTitleSection
           data= { data }
           adventure={ adventure }
